4a831a0113de04e95999e0db5cf8c77bb467db1d
LeNhatThien198 <Lenhatthien198@gmail.com>
2025-11-05 17:18:54 +0700

    chore: cấu hình route cho Admin Area trong Program.cs

--- CuaHangNhacCu/Program.cs
+++ CuaHangNhacCu/Program.cs
@@ -45,6 +45,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "AdminArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
