04ce1503514acc0e34e78fe63de142b727a805e7
Nhat Tien <ledangnhattien04@gmail.com>
2025-11-06 21:16:43 +0700

    chore: remove useless code

--- CuaHangNhacCu/Controllers/HomeController.cs
+++ CuaHangNhacCu/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace CuaHangNhacCu.Controllers
             _logger = logger;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             return View();
         }
