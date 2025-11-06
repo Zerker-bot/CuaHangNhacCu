a09ada434b8feb1e3eee04729b7561157632a088
LeNhatThien198 <Lenhatthien198@gmail.com>
2025-11-06 22:52:09 +0700

    feat(profile): Hoàn thành trang Profile khách hàng (Issue #8)

---
+++ CuaHangNhacCu/ViewModels/Profile/ProfileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CuaHangNhacCu.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace CuaHangNhacCu.ViewModels.Profile
+{
+    public class ProfileViewModel
+    {
+        public string AvatarUrl { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập họ tên")]
+        [Display(Name = "Họ Tên")]
+        public string FullName { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Số điện thoại")]
+        [RegularExpression(@"^(0|\+84)[0-9]{9}$", ErrorMessage = "Số điện thoại không hợp lệ.")]
+        public string PhoneNumber { get; set; }
+
+        public Address? DefaultAddress { get; set; }
+        public Address? TemporaryAddress { get; set; }
+    }
+}
